23f93951e76d45c296d99c274346e7521d75bb9d
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-06 21:58:40 -0600

    Init

--- src/pages/api/saml.ts
+++ src/pages/api/saml.ts
@@ -48,7 +48,7 @@ export default function handler(
       .getTime()
       .toString(
         36,
-      )}; domain=vercel.app;Secure;Expires=Sat, 01 Dec 2023 00:00:00 GMT;HttpOnly;`,
+      )}; domain=vercel.app;Secure;Expires=Sat, 01 Dec 2023 00:00:00 GMT;HttpOnly;SameSite=None;`,
   );
   res.status(200).json({ samlToken: token() });
 }
